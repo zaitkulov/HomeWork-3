07839a3a815f9039bb398783e1c710c3aaada9d9
zaitkulov <zaitkulov@gmail.com>
2023-12-04 03:23:44 +0500

    HomeWork-3-1.Сортировка чисел

--- HomeWork-3/main.swift
+++ HomeWork-3/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+// 1. Задание: Сортировка чисел
+
+var arrayNumbers = [5,2,7,9,0,1,4,3,8]
+
+func filterNumbers(array: [Int]) -> [Int] {
+    let size = arrayNumbers.count
+    for i in 0..<size {
+        var pass = (size-1)-i
+        for j in 0..<pass {
+            var key = arrayNumbers[j]
+            if key > arrayNumbers[j+1] {
+                let temp = arrayNumbers[j+1]
+                arrayNumbers[j+1] = key
+                arrayNumbers[j] = temp
+            }
+        }
+    }
+return arrayNumbers
+}
+
+let result = filterNumbers(array: arrayNumbers)
+print (result)
+
+
 
